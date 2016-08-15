1eade6630ff13c5d4b50cf2e7a6c024e8c791bd3
Dmitry Sklyarov <dima@sklyarov.com>
2016-08-15 17:14:07 +0300

    initial projects

---
+++ BlockchainMonitor.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlockchainMonitor.WebUI.MvcApplication" Language="C#" %>
---
+++ BlockchainMonitor.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlockchainMonitor.WebUI.Startup))]
+namespace BlockchainMonitor.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
